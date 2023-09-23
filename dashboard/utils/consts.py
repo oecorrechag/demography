73eca25349d06e6fde766278673e057b9ba5ad85
oecorrechag <oecorrechag@unal.edu.co>
2023-09-23 11:31:05 -0500

    dash

---
+++ dashboard/utils/consts.py
@@ -0,0 +1,7 @@
+LOGO = "assets\logo.png"
+TITLE = 'Project Name'
+
+GITHUB_PROFILE = "https://github.com/oecorrechag"
+LINKEDIN_PROFILE = "https://www.linkedin.com/in/oscar-correcha/"
+
+about_me_text= "Data scientist with 2 years experience in market segmentation, building  data-intensive applications, complex challenges in multiple industries, proficient in predictive data modeling, processing, visualizing, and extracting actionable insights from data."
